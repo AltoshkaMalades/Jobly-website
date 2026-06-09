811692dfe04fd560cd3760705d498c34f44ccfec
Altoshka <262722@almau.edu.kz>
2026-06-09 08:19:48 +0500

    fix: Add STATIC_ROOT and create static directory for production collectstatic

--- classes-main/core/settings.py
+++ classes-main/core/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # --- СТАТИКА ---
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --- БЕЗОПАСНОСТЬ (HTTPS И CSRF) ---
