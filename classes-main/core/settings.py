10d182d14d2ac08bce1158f8bf79df19ddec0583
Altoshka <262722@almau.edu.kz>
2026-06-09 09:50:05 +0500

    1ю9ю1

--- classes-main/core/settings.py
+++ classes-main/core/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 # --- СТАТИКА ---
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
